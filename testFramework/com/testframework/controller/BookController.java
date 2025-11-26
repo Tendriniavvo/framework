455a86eb4bb174d00e6cfedbca52ace8a1ce4335
Tendriniavvo <tendryniavo76@gmail.com>
2025-11-26 10:11:52 +0300

    commit

--- testFramework/com/testframework/controller/BookController.java
+++ testFramework/com/testframework/controller/BookController.java
@@ -2,6 +2,7 @@ package testFramework.com.testframework.controller;
 
 import framework.annotation.Controller;
 import framework.annotation.GetMapping;
+import framework.annotation.Param;
 import framework.utilitaire.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -21,4 +22,19 @@ public class BookController {
         mv.addObject("bookId", id);
         return mv;
     }
+
+    // Demo page to show a simple form that submits an id parameter
+    @GetMapping("/book/form")
+    public ModelAndView showForm() {
+        return new ModelAndView("/paramForm.jsp");
+    }
+
+    // Handles the form submission using @Param to bind request parameter "id" to idObjet
+    @GetMapping("/book/submit")
+    public ModelAndView submit(@Param("id") int idObjet) {
+        ModelAndView mv = new ModelAndView("/bookView.jsp");
+        mv.addObject("bookId", idObjet);
+        return mv;
+    }
 }
+
